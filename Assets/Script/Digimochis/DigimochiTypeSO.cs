b85f587e359849a1a4ea3607edd7b27ae16115a8
Nicolas Ferreiro <ferreironicolasfdi@hotmail.com>
2023-09-29 01:23:02 -0300

    Add SpriteLibraryAsset variable to DigimochiTypeSO (wip)

--- Assets/Script/Digimochis/DigimochiTypeSO.cs
+++ Assets/Script/Digimochis/DigimochiTypeSO.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 [CreateAssetMenu(fileName = "DigimochiType", menuName = "Digimochis/DigimochiTypeSO")]
 public class DigimochiTypeSO : ScriptableObject
 {
     public string digimochiTypeName;
     public Sprite digimochiMainSprite;
-    //public AnimationClip digimochiBathAnimation;
-    //public AnimationClip digimochiMedicineAnimation;
+    public SpriteLibraryAsset spriteLibraryIdleAnimation;
 }
