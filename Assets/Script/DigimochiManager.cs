8cd06613d0dc2a1925434952c07ed1bd618d138e
2box <40881833+2boxx@users.noreply.github.com>
2023-09-28 22:44:59 -0300

    Digimochi Glossary and Manager

---
+++ Assets/Script/DigimochiManager.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigimochiManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<DigimochiDataProxy> userDigimochis;
+
+    void Start()
+    {
+        userDigimochis = new List<DigimochiDataProxy>();
+        SyncUserDigimochis();
+        // Cuando termine de cargar... hacemos GetFirstDigimochi
+        // Provisoriamente
+    }
+
+    public void SyncUserDigimochis()
+    {
+        // Aquí llamas a la API o la clase que te permite obtener los Digimochis del usuario desde la blockchain.
+        // Añade cada Digimochi obtenido a la lista userDigimochis.
+    }
+
+    public DigimochiDataProxy GetFirstDigimochiData()
+    {
+        if (userDigimochis.Count > 0)
+            return userDigimochis[0];
+        else
+            return null; // El usuario no tiene Digimochis.
+    }
+}
